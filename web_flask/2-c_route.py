f673358992bd6b624d4e746c4a2ba952f280217e
Richouf95 <wourichouf95@gmail.com>
2024-04-21 21:04:37 +0000

    c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,8 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-	"""Displaye 'C' followed by the value of the text variable"""
+	"""Displaye 'C' followed by the value of
+	the text variable"""
 	text = text.replace("_", " ")	
 	return "C {}".format(text)
 
